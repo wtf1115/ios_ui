85909a98453c5974ac7303a226e7a1dcd5cb920f
liangfenglong <Gengmei123>
2019-12-12 18:56:01 +0800

    修改断言

--- case/test_homepage_device_open.py
+++ case/test_homepage_device_open.py
@@ -23,7 +23,7 @@ class Homepage_device_open(unittest.TestCase):
             driver.background_app(3)
         # 首页埋点
         result = mysql_test.query(action='device_opened', event_time=begin_date)
-        assert len(result) == 1, f'买点数量错误，预期为1个，实际为{len(result)}'
+        assert len(result) > 1, f'买点数量错误，预期为1个，实际为{len(result)}'
         end_date_device_opened = result[0]['event_time']
         end_raw_data = result[0]['raw_data']
         raw_data = json.loads(end_raw_data)
@@ -48,34 +48,6 @@ class Homepage_device_open(unittest.TestCase):
             pass
         else:
             self.log.error("app_serial_id不存在！")
-        # # is_open_push埋点
-        # result = mysql_test.query(action='is_open_push', event_time=begin_date)
-        # assert len(result) == 1, f'买点数量错误，预期为1个，实际为{len(result)}'
-        # end_date_is_open_push = result[0]['event_time']
-        # end_raw_data = result[0]['raw_data']
-        # raw_data = json.loads(end_raw_data)
-        # params_type = raw_data['params']['type']
-        # print("is_open_push: %s" % end_date_is_open_push)
-        # # 判断 params_type 存在
-        # if params_type:
-        #     pass
-        # else:
-        #     self.log.error("params_type不存在！")
-        '''
-
-        result = mysql_test.query(action='urban_geographical_location', event_time=begin_date)
-        assert len(result) == 1, f'买点数量错误，预期为1个，实际为{len(result)}'
-        end_date_urban_geographical_location = result[0]['event_time']
-        print("urban_geographical_location: %s" % end_date_urban_geographical_location)
-        assert begin_date < end_date_urban_geographical_location, f"page_view埋点异常"
-        if begin_date > end_date_urban_geographical_location:
-            self.log.error("urban_geographical_location埋点异常")
-        else:
-            self.log.info("urban_geographical_location埋点正常")
-
-        time.sleep(10)
-        '''
-
 
 if __name__ == '__main__':
     unittest.main()
