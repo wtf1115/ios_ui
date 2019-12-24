02f08bad40c6f0a2cd409925a53f57d046aad8ec
liangfenglong <Gengmei123>
2019-12-24 12:48:54 +0800

    兼容本地校验方法

--- common/getNewAPI.py
+++ common/getNewAPI.py
@@ -19,7 +19,6 @@ class Gmaddon:
         if flow.request.host in self.host_list:
             # 放在redis里面
             d = json.loads(gzip.decompress(flow.request.data.content).decode())
-            print(d)
             if isinstance(d, list):
                 [redis_gm.lpush('maidian', json.dumps(item)) for item in d]
             elif isinstance(d, dict):
